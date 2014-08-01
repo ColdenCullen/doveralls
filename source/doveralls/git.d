e87da5bbe09bfa32f436e6bf14a93e3ed0fffd1f
Colden Cullen <ColdenCullen@gmail.com>
2014-07-31 23:07:57 -0700

    Removed dependency on dlibgit

--- source/doveralls/git.d
+++ source/doveralls/git.d
@@ -3,33 +3,37 @@ import doveralls.args;
 
 auto getGitEntry( string repoPath )
 {
-    import git;
-    import std.file, std.string;
-
     CoverallsArgs.GitEntry info;
-    auto repo = openRepository( repoPath );
-    auto head = repo.lookupCommit( repo.head.target );
 
-    info.head.id = head.id.toHex();
-    info.head.author_name = head.author.name;
-    info.head.author_email = head.author.email;
-    info.head.committer_name = head.committer.name;
-    info.head.committer_email = head.committer.email;
-    info.head.message = head.message().strip();
-
-    repo.iterateBranches( GitBranchType.local, ( branch, type )
+    version( Have_dlibgit )
     {
-        if( branch.isHead )
+        import git;
+        import std.file, std.string;
+
+        auto repo = openRepository( repoPath );
+        auto head = repo.lookupCommit( repo.head.target );
+
+        info.head.id = head.id.toHex();
+        info.head.author_name = head.author.name;
+        info.head.author_email = head.author.email;
+        info.head.committer_name = head.committer.name;
+        info.head.committer_email = head.committer.email;
+        info.head.message = head.message().strip();
+
+        repo.iterateBranches( GitBranchType.local, ( branch, type )
         {
-            info.branch = branch.name;
-        }
-        return branch.isHead ? ContinueWalk.no : ContinueWalk.yes;
-    } );
+            if( branch.isHead )
+            {
+                info.branch = branch.name;
+            }
+            return branch.isHead ? ContinueWalk.no : ContinueWalk.yes;
+        } );
 
-    foreach( remoteName; repo.listRemotes() )
-    {
-        auto remote = repo.loadRemote( remoteName );
-        info.remotes ~= CoverallsArgs.GitEntry.Remote( remote.name, remote.url );
+        foreach( remoteName; repo.listRemotes() )
+        {
+            auto remote = repo.loadRemote( remoteName );
+            info.remotes ~= CoverallsArgs.GitEntry.Remote( remote.name, remote.url );
+        }
     }
 
     return info;
