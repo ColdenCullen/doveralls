da0a0bbe528e9a16aaca0ab5b02ba99e107bca5c
Martin Nowak <code@dawg.eu>
2014-11-19 00:49:27 +0100

    add -h|--help command

--- source/app.d
+++ source/app.d
@@ -3,6 +3,16 @@ import doveralls.doveralls, doveralls.config;
 
 import std.getopt, std.process, std.conv, std.file;
 
+enum usage = q"USAGE
+Usage:
+  doveralls [options]
+
+  -p|--path     repo path, default is current dir
+  -t|--token    set the repo_token, required to run locally
+  -j|--job      set the Travis-CI job id
+  --travis-pro  use travis-pro service
+USAGE";
+
 int main( string[] args )
 {
     // Default to cwd
@@ -20,6 +30,16 @@ int main( string[] args )
     void travisPro( string key, string value )
     {
         Doveralls.ciServiceName = "travis-pro";
+    }
+
+    bool help;
+    args.getopt(config.passThrough, "h|help", &help);
+
+    if( help || args.length == 1 )
+    {
+        import std.stdio;
+        writeln( usage );
+        return 0;
     }
 
     args.getopt( config.passThrough,
